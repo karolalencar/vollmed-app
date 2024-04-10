971294f930fdbc3d1cdd982ac2dcfb97563677e9
Karulina Alencar de Freitas <karulina.freitas@opus-software.com.br>
2024-04-09 22:19:35 -0300

    Add default colors

---
+++ src/utils/colors.ts
@@ -0,0 +1,15 @@
+const colors = {
+    primaryBlue: '#85C4FF',
+    secondaryBlue: '#339CFF',
+    tertiaryBlue: '#0B3B60',
+    quaternaryBlue: '#00213D',
+    error: '#893131',
+    primaryText: '#6B6E71',
+    secondaryText: '#90989F',
+    white: '#FFFFFF',
+    lightGrey: '#E7EBEF',
+    mediumGrey: '#90989F',
+    black: '#161717',
+};
+
+export default colors;
